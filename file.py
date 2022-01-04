80dcb25e28118287767669a7e1baa222525fd4e0
soheilydf <soheilydf@gmail.com>
2022-01-05 03:21:52 +0330

    commit

--- file.py
+++ file.py
@@ -1,9 +1,1 @@
 print("hello")
-print("ok")
-#this is good
-
-#you can make me laugh
-print("how about going out?")
-
-# I still think this is absolute dogshit lol
-#can you sing for me a little bit pls
